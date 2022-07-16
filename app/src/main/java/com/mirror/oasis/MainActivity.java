d3e6eef592301e8f5ef133fd8288850b788daedd
hyeonseongkang <gt9697@naver.com>
2022-07-16 23:52:41 +0900

    first commit

---
+++ app/src/main/java/com/mirror/oasis/MainActivity.java
@@ -0,0 +1,16 @@
+package com.mirror.oasis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
