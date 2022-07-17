718b5f79f57a220bfce17313a4b7a75944218502
hyeonseongkang <gt9697@naver.com>
2022-07-17 20:47:09 +0900

    t

--- app/src/main/java/com/mirror/oasis/MainActivity.java
+++ app/src/main/java/com/mirror/oasis/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     public static String TAG = "MainActivity";
+    // test
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
